a72d5a95cc6744ca45f8a0dafbde0c5248d68cc4
Caio Silva <caiocnk173@gmail.com>
2021-02-15 12:29:31 -0300

    Estrutura For

---
+++ src/main/java/cursodejava/EstruturasDeRepeticaoParte3.java
@@ -0,0 +1,26 @@
+package cursodejava;
+
+public class EstruturasDeRepeticaoParte3 {
+
+    public static void main(String[] args) {
+
+        /*
+        for(int i = 0; i <= 100; i+=10){
+            System.out.println(i);
+
+        }
+        */
+
+        for(int c = 1; c <= 3; c++){
+
+            for(int j = 0; j <= 2; j+=2){
+
+                System.out.println(c + " " + j);
+
+            }
+
+        }
+
+    }
+
+}
